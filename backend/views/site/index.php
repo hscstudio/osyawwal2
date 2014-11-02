00e5783c83b01b302ac6bd61608ac0343716c860
Fajar Wahyu <fajarwahyuabdillah@gmail.com>
2014-11-03 01:29:28 +0700

    Add new slot panel-kanan, readjusting layout accordingly

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -22,97 +22,120 @@ else
 
         <div class="row">
             
-            <div class="col-md-4">
+            <div class="col-md-10">
+
+                <div class="row">
+                    
+                    <div class="col-md-4">
+                        
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h2>Selamat datang <?php echo ucwords(Person::findOne(Yii::$app->user->identity->id)->name); ?></h2>
+                                <p>Anda memiliki 10 tugas, 5 pesan, dan 20 notifikasi yang belum dibaca</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    
+                    <div class="col-md-8">
+                        <div class="row">
+                            <div id="slot-chart-anggaran-realisasi" class="col-md-12">
+                                <?php
+                                    echo Highcharts::widget([
+                                        'scripts' => [
+                                            'modules/exporting',
+                                            'themes/grid-light',
+                                        ],
+                                        'options' => [
+                                            'title' => [
+                                                'text' => '<h3>Capaian Anggaran</h3>',
+                                                'align' => 'left',
+                                                'useHTML' => true,
+                                                'style' => [
+                                                    'font-family' => 'Source Sans Pro',
+                                                    'color' => '#676a6c',
+                                                    'text-transform' => 'capitalize',
+                                                    'font-weight' => 100
+                                                ]
+                                            ],
+                                            'subtitle' => [
+                                                'text' => '<p>Data dihitung untuk semua diklat dari semua unit tahun '.date('Y').'</p>',
+                                                'align' => 'left',
+                                                'useHTML' => true,
+                                                'style' => [
+                                                    'font-family' => 'Source Sans Pro',
+                                                    'color' => '#676a6c'
+                                                ]
+                                            ],
+                                            'xAxis' => [
+                                                'categories' => ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
+                                                'labels' => [
+                                                    'style' => [
+                                                        'font-family' => 'Source Sans Pro'
+                                                    ]
+                                                ],
+                                            ],
+                                            'yAxis' => [
+                                                'title' => [
+                                                    'enabled' => false
+                                                ],
+                                                'labels' => [
+                                                    'style' => [
+                                                        'font-family' => 'Source Sans Pro'
+                                                    ]
+                                                ],
+                                                'minorGridLineColor' => 'transparent'
+                                            ],
+                                            'credits' => [
+                                                'enabled' => false
+                                            ],
+                                            'series' => $dataSeries
+                                        ]
+                                    ]);
+                                ?>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <h4 class="angka-penting">Rp <?php echo number_format($totalAnggaran, 2, ',', '.'); ?></h4>
+                                <p class="keterangan-slot small text-muted">Total Anggaran</p>
+                            </div>
+                            <div class="col-md-4">
+                                <h4 class="angka-penting">Rp <?php echo number_format($totalRealisasi, 2, ',', '.'); ?></h4>
+                                <p class="keterangan-slot small text-muted">Total Realisasi</p>
+                            </div>
+                            <div class="col-md-4">
+                                <h4 class="angka-penting <?php echo $anggaranBahaya; ?>">Rp <?php echo number_format(abs($totalRealisasi - $totalAnggaran), 2, ',', '.'); ?></h4>
+                                <p class="keterangan-slot small text-muted">Target Anggaran yang Perlu Dikejar (Batas Aman = 70%)</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-md-2 bg-gelap panel-kanan">
                 
                 <div class="row">
+                
                     <div class="col-md-12">
-                        <h2>Selamat datang <?php echo ucwords(Person::findOne(Yii::$app->user->identity->id)->name); ?></h2>
-                        <p>Anda memiliki 10 tugas, 5 pesan, dan 20 notifikasi yang belum dibaca</p>
+                        <h4 class="margin-top-large no-bold">Pegawai Online</h4>
                     </div>
+
                 </div>
 
-            </div>
-            
-            <div class="col-md-8">
-                <div class="row">
-                    <div id="slot-chart-anggaran-realisasi" class="col-md-12">
-                        <?php
-                            echo Highcharts::widget([
-                                'scripts' => [
-                                    'modules/exporting',
-                                    'themes/grid-light',
-                                ],
-                                'options' => [
-                                    'title' => [
-                                        'text' => '<h3>Capaian Anggaran</h3>',
-                                        'align' => 'left',
-                                        'useHTML' => true,
-                                        'style' => [
-                                            'font-family' => 'Source Sans Pro',
-                                            'color' => '#676a6c',
-                                            'text-transform' => 'capitalize',
-                                            'font-weight' => 100
-                                        ]
-                                    ],
-                                    'subtitle' => [
-                                        'text' => '<p>Data dihitung untuk semua diklat dari semua unit tahun '.date('Y').'</p>',
-                                        'align' => 'left',
-                                        'useHTML' => true,
-                                        'style' => [
-                                            'font-family' => 'Source Sans Pro',
-                                            'color' => '#676a6c'
-                                        ]
-                                    ],
-                                    'xAxis' => [
-                                        'categories' => ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
-                                        'labels' => [
-                                            'style' => [
-                                                'font-family' => 'Source Sans Pro'
-                                            ]
-                                        ],
-                                    ],
-                                    'yAxis' => [
-                                        'title' => [
-                                            'enabled' => false
-                                        ],
-                                        'labels' => [
-                                            'style' => [
-                                                'font-family' => 'Source Sans Pro'
-                                            ]
-                                        ],
-                                        'minorGridLineColor' => 'transparent'
-                                    ],
-                                    'credits' => [
-                                        'enabled' => false
-                                    ],
-                                    'series' => $dataSeries
-                                ]
-                            ]);
-                        ?>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-4">
-                        <h4 class="angka-penting">Rp <?php echo number_format($totalAnggaran, 2, ',', '.'); ?></h4>
-                        <p class="keterangan-slot small text-muted">Total Anggaran</p>
-                    </div>
-                    <div class="col-md-4">
-                        <h4 class="angka-penting">Rp <?php echo number_format($totalRealisasi, 2, ',', '.'); ?></h4>
-                        <p class="keterangan-slot small text-muted">Total Realisasi</p>
-                    </div>
-                    <div class="col-md-4">
-                        <h4 class="angka-penting <?php echo $anggaranBahaya; ?>">Rp <?php echo number_format(abs($totalRealisasi - $totalAnggaran), 2, ',', '.'); ?></h4>
-                        <p class="keterangan-slot small text-muted">Target Anggaran yang Perlu Dikejar (Batas Aman = 70%)</p>
-                    </div>
-                </div>
             </div>
 
         </div>
+
 
     </div>
 </div>
 <?php
     $this->registerCss('
+        .container-fluid .right-side > .content {
+            padding: 0px;
+        }
         .site-index .container-fluid {
             padding:17px;
         }
