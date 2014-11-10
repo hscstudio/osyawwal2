085f97bbfd0d3fbbc61854f320dbdf43d19a9cac
Hafid Mukhlasin <milisstudio@gmail.com>
2014-11-10 15:42:00 +0700

    Enhance update, and import employee.. add related field

--- backend/modules/pusdiklat/general/views/assignment0/update.php
+++ backend/modules/pusdiklat/general/views/assignment0/update.php
@@ -8,7 +8,6 @@ use yii\helpers\Html;
 $controller = $this->context;
 $menus = $controller->module->getMenuItems();
 $this->params['sideMenu'][$controller->module->uniqueId]=$menus;
-
 $this->title = Yii::t('app', 'Update {modelClass}: ', [
     'modelClass' => 'User',
 ]) . ' ' . $model->id;
@@ -26,6 +25,23 @@ $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 		<h1 class="panel-title"><?= Html::encode($this->title) ?></h1>
 	</div>
 	<div class="panel-body">
+		<?php
+		$position = 'Pelaksana';
+		if (@$model->employee->chairman>0){
+			$position = @$model->employee->person->position;
+		}
+		?>
+		<table class="table table-striped table-condensed">
+		<tr>
+			<td>Nama </td><td>:</td><td><?= @$model->employee->person->name; ?></td>
+		</tr>
+		<tr>
+			<td>Jabatan </td><td>:</td><td><?= $position; ?> </td>
+		</tr>
+		<tr>
+			<td>Bagian/Bidang </td><td>:</td><td><?= @$model->employee->organisation->NM_UNIT_ORG; ?> </td>
+		</tr>
+		</table>
 		<?= $this->render('_form', [
 			'model' => $model,
 		]) ?>
