08811858d88ee03d6da25f88b6bde6972b7d0127
Fajar Wahyu <fajarwahyuabdillah@gmail.com>
2014-11-14 02:40:25 +0700

    Clone manajemen pengajar dari pusdiklat

---
+++ backend/modules/bdk/execution/views/trainer3/_search.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\modules\bdkexecution\models\TrainerSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="trainer-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'person_id') ?>
+
+    <?= $form->field($model, 'category') ?>
+
+    <?= $form->field($model, 'education_history') ?>
+
+    <?= $form->field($model, 'training_history') ?>
+
+    <?= $form->field($model, 'experience_history') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
