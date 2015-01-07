379127deab4cf2892ba6eca21681dff2eaa309fd
muhamadg <muhamadg@gmail.com>
2015-01-07 16:49:09 +0700

    add reset password student

---
+++ backend/modules/admin/views/student/index.php
@@ -0,0 +1,84 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView as Gridview2;
+use kartik\grid\GridView;
+use yii\helpers\Url;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\UserSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$controller = $this->context;
+$menus = $controller->module->getMenuItems();
+$this->params['sideMenu'][$controller->module->uniqueId]=$menus;
+$this->title = Yii::t('app', 'Students');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-index">
+	
+<!--
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create {modelClass}', [
+    'modelClass' => 'Student',
+]), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+-->
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'kartik\grid\SerialColumn'],
+
+            [
+				'label' => 'name',				
+				'vAlign'=>'middle',
+				'hAlign'=>'center',
+				'headerOptions'=>['class'=>'kv-sticky-column'],
+				'contentOptions'=>['class'=>'kv-sticky-column'],
+				'format'=>'raw',
+				'value' => function ($data){
+					return Html::tag('span', $data->person->name); 
+				},				
+			],
+			
+			[
+				'attribute' => 'username',
+				'vAlign'=>'middle',
+				'hAlign'=>'center',
+				'headerOptions'=>['class'=>'kv-sticky-column'],
+				'contentOptions'=>['class'=>'kv-sticky-column'],					
+			],
+            
+            //'email:email',       
+            
+			[
+				'attribute' => 'status',
+				'vAlign'=>'middle',
+				'hAlign'=>'center',
+				'width' => '100px',
+				'headerOptions'=>['class'=>'kv-sticky-column'],
+				'contentOptions'=>['class'=>'kv-sticky-column'],
+				'format' => 'raw',
+				'value' => function ($data){
+					return Html::tag('span',($data->status==1)?'Active':'Blocked',['class'=>($data->status==1)?'label label-info':'label label-warning']);
+				}
+			],
+			
+            ['class' => 'kartik\grid\ActionColumn'],
+        ],
+		'panel' => [
+			'heading'=>'<h3 class="panel-title"><i class="fa fa-fw fa-globe"></i> '.Html::encode($this->title).'</h3>',
+			'before'=>'',
+			'after'=>Html::a('<i class="fa fa-fw fa-repeat"></i> Reset Grid', Url::to(''), ['class' => 'btn btn-info']),
+			'showFooter'=>false
+		],
+		'responsive'=>true,
+		'hover'=>true,
+    ]); ?>
+	<?= \hscstudio\heart\widgets\Modal::widget(['modalSize'=>'']) ?>
+</div>
